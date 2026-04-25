6d8d9a601f17c9e82dea0ee0fc3e40057f191b5f
Julio-Leal <cavalcantejulio522@gmail.com>
2026-04-25 15:28:41 -0300

    Altera: Escrita de alguns trechos

--- src/main/java/br/com/servidor/ServidorMain.java
+++ src/main/java/br/com/servidor/ServidorMain.java
@@ -11,27 +11,27 @@ public class ServidorMain {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("▶️ Servidor iniciando...");
+		System.out.println("▶️ - Servidor iniciando...");
 		
 		AuthService authService = new AuthService();
 		
 		try(ServerSocket servidor = new ServerSocket(PORTA_PADRAO)) {
 			
-			System.out.println("✅ Servidor rodando na porta " + PORTA_PADRAO);
+			System.out.println("✅ - Servidor rodando na porta " + PORTA_PADRAO);
 			
 			while(true) {
-				System.out.println("⌛ Aguardando cliente...");
+				System.out.println("⌛ - Aguardando cliente...");
 				
 				Socket clienteSocket = servidor.accept();
 				
-				System.out.println("🔌 Cliente conectado: " + clienteSocket.getInetAddress());
+				System.out.println("🔌 - Cliente conectado: " + clienteSocket.getInetAddress());
 				
 				ClienteHandler handler = new ClienteHandler(clienteSocket, authService);
 				
 				new Thread(handler).start();
 			}
 		} catch (Exception e) {
-			System.out.println("❌ Erro no servidor:");
+			System.out.println("❌ - Erro no servidor:");
 			e.printStackTrace();
 		}
 	}

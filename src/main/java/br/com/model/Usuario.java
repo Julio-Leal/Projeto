4449e5b0d8f6546c73e9e5ca6350fae09624f4bb
Julio-Leal <cavalcantejulio522@gmail.com>
2026-04-27 00:05:51 -0300

    Adiciona: Novos atributos na classe Usuario

--- src/main/java/br/com/model/Usuario.java
+++ src/main/java/br/com/model/Usuario.java
@@ -10,16 +10,34 @@ public class Usuario {
 		this.usuario = usuario;
 		this.senha = senha;
 	}
+	
+	public Usuario() {
+		
+	}
 
 	public String getNome() {
 		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
 	public String getUsuario() {
 		return usuario;
 	}
 
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
 	public String getSenha() {
 		return senha;
 	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+
 }

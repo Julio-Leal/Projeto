56aaf2a0cbe7dfadf2ba47b35a441338d28ac3ac
Julio-Leal <cavalcantejulio522@gmail.com>
2026-05-02 22:21:55 -0300

    Delete: Exclui campos comentados e nao utilizados na classe

--- src/main/java/br/com/model/Mensagem.java
+++ src/main/java/br/com/model/Mensagem.java
@@ -10,10 +10,6 @@ public class Mensagem {
 	private String mensagem;
 	private String resposta;
 	
-//	private String novoNome;
-//	private String novoUsuario;
-//	private String novaSenha;
-	
 	public Mensagem(String op, String nome, String usuario, String senha, String token, String destinatario,
 					String mensagem, String resposta) {
 		this.op = op;
@@ -93,28 +89,4 @@ public class Mensagem {
 	public void setResposta(String resposta) {
 		this.resposta = resposta;
 	}
-	
-//	public String getNovoNome() {
-//		return novoNome;
-//	}
-	
-//	public void setNovoNome(String novoNome) {
-//		this.novoNome = novoNome;
-//	}
-	/* TURMA NÃO ESTÁ UTILIZANDO 🚩
-	public String getNovousuario() {
-		return novoUsuario;
-	}
-	
-	public void setNovoUsuario(String novoUsuario) {
-		this.novoUsuario = novoUsuario;
-	}
-	*/
-//	public String getNovaSenha() {
-//		return novaSenha;
-//	}
-	
-//	public void setNovaSenha(String novaSenha) {
-//		this.novaSenha = novaSenha;
-//	}
 }
